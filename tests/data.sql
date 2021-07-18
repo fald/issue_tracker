38d0995cd710316634f7d2526aa2704bb89d21aa
Firas <firasaldabbas+git@gmail.com>
2021-07-18 12:55:24 -0400

    Bugfixing: Capitalization issue and sample SQL file, since I forgot syntax.

--- tests/data.sql
+++ tests/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO user (username, password)
 VALUES 
     ('fald', null),
-    ('zuse', 'aaa'). -- lol plaintext passwords because no auth
+    ('zuse', 'aaa'), -- lol plaintext passwords because no auth
     ('russ', null);
 
 INSERT INTO project (name, description)
 VALUES
     ('Bug Tracker', 'Track bugs across many projects.'),
-    ('Project Manager/"', 'Manage many projects'),
+    ('Project Manager/', 'Manage many projects'),
     ('Delver', 'Rogue-lite using a hex-grid and tactical turns.'),
     ('EDA Manager', 'Exploratory Data Analysis manager to aid in creating and completing such projects.');
 
@@ -15,6 +15,6 @@ INSERT INTO issue
     (project_id, title, body, creator_id, target_id, status, priority, created, last_modified)
 VALUES
     (1, 'SQL Issues', 'Building SQL queries in Python is ass.', 1, null, 'Open', 'Low', '2016-01-02 00:00:01', CURRENT_TIMESTAMP),
-    (1, 'Auth issues', 'Auth module does not work and is not required to edit issues.', 2, 3, 'Open', 'Medium', CURRENT_TIMESTAMP),
-    (2, 'No project outline', 'No one knows what the expectations or final product are!!!', 3, null, 'Open', 'High'),
-    (3, 'FOV System broken', 'Not intended to work with hex-grid.', 1, 1, 'Open', 'Low');
+    (1, 'Auth issues', 'Auth module does not work and is not required to edit issues.', 2, 3, 'Open', 'Medium', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, 'No project outline', 'No one knows what the expectations or final product are!!!', 3, null, 'Open', 'High', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3, 'FOV System broken', 'Not intended to work with hex-grid.', 1, 1, 'Open', 'Low', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
