8192e3bb60b7268c8f4dc550dc3016f384c5f47b
Firas <firasaldabbas+git@gmail.com>
2021-07-18 00:48:40 -0400

    Index view - little bit of being awful by not having the actual template in yet, but whatever

--- tracker/bugtracker.py
+++ tracker/bugtracker.py
@@ -7,3 +7,30 @@ from tracker.db import get_db
 
 
 bp = Blueprint('bugtracker', __name__)
+
+
+@bp.route('/')
+def index():
+    db = get_db()
+    bugs = db.execute(
+        'SELECT'
+        '   i.id AS id'
+        '   i.title'
+        '   i.body'
+        '   i.status'
+        '   i.priority'
+        '   i.created'
+        '   i.last_modified'
+        '   p.name AS project'
+        '   u1.username AS creator'
+        '   u2.username AS target'
+        'FROM'
+        '   issue i'
+        '   LEFT JOIN project p ON i.project_id = p.id'
+        '   LEFT JOIN user u1 ON i.creator_id = u1.id'
+        '   LEFT JOIN user u2 ON i.target_id = u2.id'
+        'ORDER BY'
+        '   created DESC'
+    ).fetchall()
+    # ITS PROBABLY GREAT THAT I GO BETWEEN TRACKER, BUGS, BUGTRACKER AND ISSUES RIGHT NO PROBLEMS THERE HAHAH IM THE BEST - t. Late-night me
+    return render_template('/bugs/index.html', bugs=bugs)
