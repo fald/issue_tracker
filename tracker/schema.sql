e82bbdf7751bf5fde49d9f6d1590d9f6fee5dbba
Firas <firasaldabbas+git@gmail.com>
2021-07-17 23:18:22 -0400

    Initial application factory in, initial schema and db files in.

---
+++ tracker/schema.sql
@@ -0,0 +1,43 @@
+/* 
+Tables:
+    Issue
+        ID, FK Project, Title, Body, FK Creator,
+        FK Assignee, Status, Priority,
+        Creation date, Last modified date
+    Project (Simple)
+        ID, Name, Description
+    User
+        ID, Username, Password
+*/
+
+DROP TABLE IF EXISTS issue;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT -- This can be null for now for unregistered users
+);
+
+CREATE TABLE project (
+    if INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE issue (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    creator_id INTEGER NOT NULL DEFAULT "fald",
+    target_id INTEGER,
+    status TEXT DEFAULT "open",
+    priority TEXT DEFAULT "low",
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (project_id) REFERENCES project (id),
+    FOREIGN KEY (creator_id) REFERENCES user (id),
+    FOREIGN KEY (target_id) REFERENCES user (id)
+);
