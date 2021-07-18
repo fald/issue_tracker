ff3c0513b1f753a2aace58c255eaaa6e02d7bc50
Firas <firasaldabbas+git@gmail.com>
2021-07-17 23:42:33 -0400

    Updated some bug blueprint stuff - main feature, so no prefix necessary, and no password hashing because it isn't the auth...what was I even thinking?

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -26,4 +26,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import bugtracker
+    app.register_blueprint(bugtracker.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
--- tracker/bugtracker.py
+++ tracker/bugtracker.py
@@ -1,11 +1,9 @@
-import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug import abort
 
 from tracker.db import get_db
 
 
-bp = Blueprint('bugtracker', __name__, url_prefix='/bugs')
+bp = Blueprint('bugtracker', __name__)
