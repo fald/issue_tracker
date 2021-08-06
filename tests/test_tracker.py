0fb6ed726617ead5f6d2184a71ac45f9e833ab8d
Firas <4348649+fald@users.noreply.github.com>
2021-08-06 00:11:15 -0400

    Testing: creating and reading tests in place.

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -27,12 +27,12 @@ def client():
     os.unlink(db_path)
 
 
-get_pages = [
+get_pages = (
     ('', b'There are no bugs being tracked right now.'),
     ('test', b'Hello, Dev!'),
     ('create', b'New Bug - Issue Tracker'),
     ('search', b'Search - Issue Tracker')
-]
+)
 
 
 @pytest.mark.parametrize('page, text', get_pages)
@@ -42,60 +42,95 @@ def test_get_base_pages(client, page, text):
     assert text in response.data
 
 
-        # project_name = request.form['project_name']     
-        # bug_title = request.form['bug_title']
-        # bug_description = request.form['bug_description']   
-        # creator_name = request.form['creator_name']
-        # assignee = request.form['assignee']
-        # status = request.form['status']
-        # priority = request.form['priority']
+add_issues = (
+    # Full
+    (
+        {
+            'project_name': 'Project Name',
+            'bug_title': 'Bug Title',
+            'bug_description': 'Bug Description',
+            'creator_name': 'Creator Name',
+            'assignee': 'Assignee',
+            'status': 'Status',
+            'priority': 'Priority'
+        }, 
+        302,
+        'http://localhost/'
+    ),
+    # Required-only
+    (
+        {
+            'project_name': 'Project Name',
+            'bug_title': 'Bug Title',
+            'bug_description': 'Bug Description',
+        }, 
+        302,
+        'http://localhost/'
+    ),
+    # Incomplete
+    (
+        {
+            'project_name': 'Project Broken',
+            'creator_id': 'Jimboni'
+        },
+        200,
+        None
+    ),
+    # Required...but wrong
+    (
+        {
+            'project_nam': 'Dropped my e',
+            'bug_header': 'That makes more sense',
+            'bug_details': 'So does that'
+        },
+        200,
+        None
+    ),
+    # Nothin'
+    (
+        {},
+        200,
+        None
+    )
+)
 
-add_issues = [
- (
-     {
-         'project_name': 'Project Name',
-         'bug_title': 'Bug Title',
-         'bug_description': 'Bug Description',
-         'creator_name': 'Creator Name',
-         'assignee': 'Assignee',
-         'status': 'Status',
-         'priority': 'Priority'
-     }, 
-     302,
-     'http://localhost/'
- ),
-#  (
-#      {
-
-#      }, 
-#      200
-#  )
-]
-# Test add full
 @pytest.mark.parametrize('data, expected_response, expected_location', add_issues)
 def test_add_message(client, data, expected_response, expected_location):
     # Response works => redirect works => 302, not 200!
     response = client.post('/create', data=data)
     assert response.status_code == expected_response
+    # if expected_response == 302:
     assert response.location == expected_location
 
 
-# Test add partial/required-only
-
-
-# Test add bad/incomplete
-
-
-# Test add wrong input names
-
-
-# Test add empty
-
-
 # Test read non-empty index
+def test_read_nonempty(client):
+    assert b'There are no bugs being tracked right now' in client.get('/').data
+    client.post('/create', data=add_issues[0][0])
+    assert b'There are no bugs being tracked right now' not in client.get('/').data
 
+
+search_terms = (
+    ('fald', b'Showing 1 result for'),
+    ('Project', b'Showing 2 results for'),
+    ('project', b'Showing 2 results for'),
+    ('asdadakca', b'There were 0 results for your search term.')
+)
 
 # Test search term
+@pytest.mark.parametrize('term, expected', search_terms)
+def test_search(client, term, expected):
+    # Base search page, no messages
+    rd = client.get('/search').data
+    assert b'There were' not in rd
+    assert b'Showing ' not in rd
+
+    # Adding some stuff to db
+    client.post('/create', data=add_issues[0][0])
+    client.post('/create', data=add_issues[1][0])
+
+    rd = client.get(f'/search?search_term={term}')
+    assert expected in rd.data
 
 
 # Test update an issue; one field
